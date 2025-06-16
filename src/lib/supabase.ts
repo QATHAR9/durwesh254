6e2f96a63f2dccda41d1d4b120b84d2d3fbf1219
QATHAR9 <158095381+QATHAR9@users.noreply.github.com>
2025-06-16 13:02:08 +0300

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://tldleqjobqryohastvem.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsZGxlcWpvYnFyeW9oYXN0dmVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk1MzYzMTcsImV4cCI6MjA2NTExMjMxN30.3lqVcvVRJoFkRYxy_jHNcbxHiRh4GPaWQAxp8VbSse0
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsZGxlcWpvYnFyeW9oYXN0dmVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk1MzYzMTcsImV4cCI6MjA2NTExMjMxN30.3lqVcvVRJoFkRYxy_jHNcbxHiRh4GPaWQAxp8VbSse0'';
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export interface Product {
